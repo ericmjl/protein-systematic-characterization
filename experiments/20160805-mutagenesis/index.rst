803bf86a2896ec38e0728c8063c9977f67f15809
Eric J. Ma <ericmajinglong@gmail.com>
2016-09-08 09:24:04 -0400

    made python script that builds index for a directory

---
+++ experiments/20160805-mutagenesis/index.rst
@@ -0,0 +1,15 @@
+Index automatically generated using make_index.py.
+
+Reports:
+
+- `20160805-mutagenesis-report.md <./20160805-mutagenesis-report.md>`_ 
+- `20160812-clone-pCI-report.md <./20160812-clone-pCI-report.md>`_ 
+- `20160815-mutate-pb2-report.md <./20160815-mutate-pb2-report.md>`_ 
+- `20160815-troubleshoot-pb2.md <./20160815-troubleshoot-pb2.md>`_ 
+- `20160817-control-pcr.md <./20160817-control-pcr.md>`_ 
+- `20160825-primer-test.md <./20160825-primer-test.md>`_ 
+- `20160829-gradient-pcr.md <./20160829-gradient-pcr.md>`_ 
+- `20160831-mutagenesis.md <./20160831-mutagenesis.md>`_ 
+- `20160902-mutagenesis-2.md <./20160902-mutagenesis-2.md>`_ 
+- `20160902-mutagenesis-pb2.md <./20160902-mutagenesis-pb2.md>`_ 
+- `20160906-DMSO-mutagenesis.md <./20160906-DMSO-mutagenesis.md>`_ 
---
+++ experiments/make_index.py
@@ -0,0 +1,52 @@
+import os
+import click
+from pypandoc import convert_file
+
+def get_md_files(directory):
+    """
+    Iterates over all files in a directory and returns a list of markdown
+    files that are present in that directory.
+    """
+    md_files = []
+
+    for f in os.listdir(directory):
+        if f.endswith(('.md', '.MD', '.markdown', '.mdown')):
+            md_files.append(f)
+
+    return md_files
+
+def write_index(directory, md_files):
+    """
+    Writes the index to a text file. It is markdown-formatted, but ends with
+    '.rst' in order to no confuse the get_md_files function.
+
+    Parameter types:
+    ================
+    - directory: str
+    - md_files: list of str
+    """
+    with open('{0}/index.rst'.format(directory), 'w') as f:
+        f.write('Index automatically generated using make_index.py.\n\n')
+        f.write('Reports:\n\n')
+        for md in md_files:
+            f.write('- `{0} <./{0}>`_ \n'.format(md))
+
+
+def convert_index_to_html(directory):
+    """
+    Looks for the index.rst file, and converts it to index.html using pypandoc.
+    """
+    convert_file('{0}/index.rst'.format(directory),
+                 'html',
+                 outputfile='{0}/index.html'.format(directory))
+
+@click.command()
+@click.option('--directory')
+def build_index(directory):
+    # Get the markdown files from a directory
+    md_files = get_md_files(directory)
+    write_index(directory, md_files)
+    convert_index_to_html(directory)
+
+if __name__ == '__main__':
+    build_index()
