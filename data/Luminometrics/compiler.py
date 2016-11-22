569e0677024b0aa23eb3069619d2a70faf40b60d
Eric J. Ma <ericmajinglong@gmail.com>
2016-11-22 15:55:59 -0500

    script fixes
    1. ‘file’ is a reserved keyword in python, changed to ‘f’.
    2. each line should be < 79 characters - to fit pycodestyle

--- data/Luminometrics/compiler.py
+++ data/Luminometrics/compiler.py
@@ -8,14 +8,17 @@ import pandas as pd
 import os
 
 files = os.listdir()
-master_df = pd.DataFrame(columns = ["SampleID", "Well", "Replicate", "Well Type", "Date", "Luminescence", "Normalized to Negative", "Vlumins"])
-for file in files:
-    if file.endswith(".csv") and file != "master_datafile.csv":
-        df = pd.read_csv(file, index_col = "SampleID")
+master_df = pd.DataFrame(columns=["SampleID", "Well", "Replicate", "Well Type",
+                                  "Date", "Luminescence",
+                                  "Normalized to Negative", "Vlumins"])
+for f in files:
+    if f.endswith(".csv") and f != "master_datafile.csv":
+        print(f)
+        df = pd.read_csv(f, index_col = "SampleID")
         df["Luminescence"] = df["Luminescence"] + 1 - df["Luminescence"].min()
         #Negative control normalization to a Mock test
         df["Normalized to Negative"] = df["Luminescence"]/df["Luminescence"]["Mock"].values[0]
-        #Positive control normalization, calculates the normalized polymerase activity relative to pol activity in the Vic control 
+        #Positive control normalization, calculates the normalized polymerase activity relative to pol activity in the Vic control
         try:
             df["Vlumins"] = df["Normalized to Negative"]/df["Normalized to Negative"]["Vic"].values[0]
         except:
@@ -24,4 +27,3 @@ for file in files:
         master_df = master_df.append(df, ignore_index=True)
 #Overwrite master file with new compiled dataframe
 master_df.to_csv("master_datafile.csv")
-    
